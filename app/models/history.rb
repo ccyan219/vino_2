507b527b20501905e2fd52290d11c6a8fb01eb06
Raghu Betina <raghu@firstdraft.com>
2018-06-16 23:56:22 +0000

    Direct association between listings and histories

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,6 +1,8 @@
 class History < ApplicationRecord
   # Direct associations
 
+  belongs_to :listing
+
   # Indirect associations
 
   # Validations
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :histories,
+             :dependent => :destroy
+
   belongs_to :wine
 
   # Indirect associations
