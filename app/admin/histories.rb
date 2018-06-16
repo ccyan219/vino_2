32e6272f2793390666c5e3534ebb01299507f542
Raghu Betina <raghu@firstdraft.com>
2018-06-16 23:56:42 +0000

    Whitelist columns in History for ActiveAdmin

--- app/admin/histories.rb
+++ app/admin/histories.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register History do
+
+ permit_params :user_id, :listing_id, :comments, :rating, :favorite
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
